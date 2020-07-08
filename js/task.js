fb87ed75780cadb0066ea591d25dbb51f02736d6
llenn4ik <57867092+llenn4ik@users.noreply.github.com>
2020-07-08 12:23:56 +0300

    add eventListener

--- js/task.js
+++ js/task.js
@@ -25,12 +25,19 @@ const createListImages = images.map((im) => {
 galleryImagesList.append(...createListImages);
 console.dir(galleryImagesList);
 //  модальное окно
-const lightbox = galleryImagesList.querySelector('.js-lightbox');
-const lightbox_img = lightbox.querySelector('.lightbox__image')
+const lightbox = document.querySelector('.lightbox');
+const lightbox_img = document.querySelector('.lightbox__image')
 // open modal window
-lightbox.addEventListener('click', event => {
-  lightbox.classList.add('.is-open');
-  changeImgSrc(el);
+console.log('lightbox', lightbox);
+console.log('lightbox_img', lightbox_img);
+
+console.log('galleryImagesList', galleryImagesList)
+
+galleryImagesList.addEventListener('click', event => {
+  event.preventDefault();
+  lightbox.classList.add('is-open');
+  changeImgSrc(event.target);
+  console.log('+++++++')
 });
 
 function changeImgSrc(el) {
@@ -38,16 +45,52 @@ function changeImgSrc(el) {
 };
 // close modal window
 function closeModalWindow() {
-  lightbox.classList.remove('.is-open');
+  lightbox.classList.remove('is-open');
 
-  lightbox_img.setAttribute('src', '${im.preview}')
+  lightbox_img.setAttribute('src', '');
+  console.log('-+-+-')
 };
 // слушатель на overlay, esc, button:
-const overlay = galleryImagesList.querySelector('.lightbox__overlay')
+const overlay = document.querySelector('.lightbox__overlay');
+
+console.log('overlay', overlay);
 overlay.addEventListener('click', handleOverlayCloseWindow)
 
-function handleOverlayCloseWindow(target, currentTarget) {
+
+
+function handleOverlayCloseWindow({
+  target,
+  currentTarget
+}) {
+  console.log('target', target);
+  console.log("currentTarget", currentTarget);
   if (target === currentTarget) {
-    closeModalWindow()
+    closeModalWindow();
+    console.log('---')
   }
-}
+};
+window.addEventListener('keydown', handleEsc)
+console.log(handleEsc);
+
+function handleEsc(event) {
+  if (event.code === 'Escape') {
+    closeModalWindow()
+
+  }
+};
+const button = document.querySelector('.lightbox__button');
+button.addEventListener('click', handleButton)
+
+function handleButton({
+  target,
+  currentTarget
+}) {
+  console.log('target', target);
+  console.log("currentTarget", currentTarget);
+  if (target === currentTarget) {
+
+
+    closeModalWindow();
+    console.log('click')
+  }
+};
